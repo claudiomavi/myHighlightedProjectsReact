cd56f65124f2e2dcd1444f47742f58cb1a8e39fa
Claudio <claudio.marino@outlook.com>
2024-07-21 22:48:48 +0200

    updated with new project

--- data.js
+++ data.js
@@ -1,23 +1,23 @@
 export default [
 	{
-		key: 1,
-		title: "página empresa my system",
-		lenguages: "html-css-javascript",
-		date: "15 Mar, 2024",
+		key: 5,
+		title: "tenzie game",
+		lenguages: "html-css-javascript-react",
+		date: "21 Jul, 2024",
 		description:
-			"Página web principal para una empresa italiana de mosquiteras. Falta material para acabarla pero en agosto tengo reunión con ellos para aclarar los últimos detalles.",
-		imageUrl: "https://i.imgur.com/6Kc0ccZ.jpg",
-		siteUrl: "https://mysystemnapoli.netlify.app/",
+			"Juego muy sencillo donde he aplicado mis últimos aprendizajes de React.",
+		imageUrl: "https://imgur.com/FodhjYd.jpg",
+		siteUrl: "https://tenzieclaudio.netlify.app/",
 	},
 	{
-		key: 2,
-		title: "calculadora",
-		lenguages: "html-css-javascript",
-		date: "16 Abr, 2024",
+		key: 4,
+		title: "shopping app",
+		lenguages: "html-css-javascript-firebase",
+		date: "4 Jun, 2024",
 		description:
-			"Calculadora hecha como tarea para mi grado superior, a parte de las funcionalidades me centré mucho en el diseño",
-		imageUrl: "https://i.imgur.com/s2qRUQN.jpg",
-		siteUrl: "https://claudiocalculadora.netlify.app/",
+			"Aplicación también para móvil donde puedes compartir en directo tu lista de la compra con otras personas.",
+		imageUrl: "https://i.imgur.com/g4M5QrD.jpg",
+		siteUrl: "https://shoppingclaudiodemo.netlify.app/",
 	},
 	{
 		key: 3,
@@ -30,13 +30,23 @@ export default [
 		siteUrl: "https://taniamirasolanton.netlify.app/",
 	},
 	{
-		key: 4,
-		title: "shopping app",
-		lenguages: "html-css-javascript-firebase",
-		date: "4 Jun, 2024",
+		key: 2,
+		title: "calculadora",
+		lenguages: "html-css-javascript",
+		date: "16 Abr, 2024",
 		description:
-			"Aplicación también para móvil donde puedes compartir en directo tu lista de la compra con otras personas.",
-		imageUrl: "https://i.imgur.com/g4M5QrD.jpg",
-		siteUrl: "https://shoppingclaudiodemo.netlify.app/",
+			"Calculadora hecha como tarea para mi grado superior, a parte de las funcionalidades me centré mucho en el diseño",
+		imageUrl: "https://i.imgur.com/s2qRUQN.jpg",
+		siteUrl: "https://claudiocalculadora.netlify.app/",
+	},
+	{
+		key: 1,
+		title: "página empresa my system",
+		lenguages: "html-css-javascript",
+		date: "15 Mar, 2024",
+		description:
+			"Página web principal para una empresa italiana de mosquiteras. Falta material para acabarla pero en agosto tengo reunión con ellos para aclarar los últimos detalles.",
+		imageUrl: "https://i.imgur.com/6Kc0ccZ.jpg",
+		siteUrl: "https://mysystemnapoli.netlify.app/",
 	},
 ];
